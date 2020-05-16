2cc10ccb553504a4873a062764758c89d7080458
RAM García <magase98@gmail.com>
2020-05-16 20:35:57 +0200

    Cambiar visibilidad

--- facturas/Facturas.java
+++ facturas/Facturas.java
@@ -7,26 +7,29 @@ public class Facturas implements IFacturas {
 		int num;
 		float base;
 		float tipoIva;
-		Estados estado;
+		protected Estados estado;
 		
 	}
 
-	
+
 	public void cambiarEstado(Estados estado) {
-		
+		// TODO Auto-generated method stub
 		
 	}
 
-	
+
 	public float getTotal() {
-		
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	public float getCantidadIva() {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	
-	public float getCantidadIva() {
-		
-		return 0;
-	}
+	
 	
 }
