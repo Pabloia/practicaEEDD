bf14187d4dca0606dfbfe08c473ac72bdefe8677
RAM García <magase98@gmail.com>
2020-05-21 10:50:25 +0200

    Correccion del Bug
    Cambiar visibilidad

--- facturas/Facturas.java
+++ facturas/Facturas.java
@@ -1,17 +1,17 @@
 package facturas;
 
 private class Facturas implements IFacturas {
-	
+	//Correccion del Bug 
 	public static void main (String [] args)  {
 		
 		int num;
 		float base;
 		float tipoIva;
-		protected Estados estado;
+		private Estados estado;
 		
 	}
 
-
+	
 	public void cambiarEstado(Estados estado) {
 		// TODO Auto-generated method stub
 		
